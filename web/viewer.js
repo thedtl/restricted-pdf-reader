ca8cd48caa3959e7e9be4053ff027d5609bd204d
crawby <ccrawford@thedtl.org>
2025-12-02 14:46:08 -0700

    Disable Security Check

--- web/viewer.js
+++ web/viewer.js
@@ -1643,8 +1643,8 @@ exports.PDFViewerApplication = PDFViewerApplication;
       }
       const fileOrigin = new URL(file, window.location.href).origin;
       if (fileOrigin !== viewerOrigin) {
-        throw new Error("file origin does not match viewer's");
-      }
+  // throw new Error("file origin does not match viewer's");
+}
     } catch (ex) {
       PDFViewerApplication.l10n.get("loading_error").then(msg => {
         PDFViewerApplication._documentError(msg, {
